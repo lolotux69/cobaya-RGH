8272990bfb33a18c3e152d1f3d0e5f53dfc3bb3c
lolo <lolo@debian12>
2025-11-11 20:07:20 +0100

    ✨ Ajout du module RGHLike avec emcee et comparaison Planck

--- create-cobay-env.sh
+++ create-cobay-env.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 # 1. Crée un environnement
 python3 -m venv cobaya_env
 source cobaya_env/bin/activate
 # 2. Mets pip à jour
 pip install --upgrade pip
 # 3. Installe Cobaya (stable)
-pip install cobaya
+pip install cobaya emcee matplotlib
